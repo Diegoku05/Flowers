e9db16eb7138f00b43754560b8ee141e623d4dbf
Diegoku05 <diegoandressilvam05@gmail.com>
2025-02-22 17:44:22 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 onload = () =>{
         document.body.classList.remove("container");
 };
+onload = () => {
+    // Eliminar la clase "container" del body
+    document.body.classList.remove("container");
+
+    // Configurar el audio
+    const audio = document.getElementById("audio");
+
+    // Reproducir el audio con sonido después de la interacción del usuario
+    document.addEventListener("click", () => {
+        audio.muted = false; // Habilitar el sonido
+        audio.play(); // Reproducir el audio
+    });
+};
