1af427de584355983df445e947cc715ef8e8b268
Diegoku05 <diegoandressilvam05@gmail.com>
2025-02-22 19:36:58 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-// Esperar a que la página se cargue completamente
-window.onload = () => {
+onload = () => {
     // Eliminar la clase "container" del body
     document.body.classList.remove("container");
 
@@ -8,19 +7,12 @@ window.onload = () => {
 
     // Reproducir el audio con sonido después de la interacción del usuario
     document.addEventListener("click", () => {
-        if (audio.paused) { // Si el audio está pausado
+        if (audio.paused) { // Verificar si el audio está pausado
             audio.muted = false; // Habilitar el sonido
             audio.play(); // Reproducir el audio
         }
     });
 
-    // Cargar contenido dinámico al hacer clic en el botón
-    document.getElementById("cargarContenido").addEventListener("click", () => {
-        fetch("FLORES.html")
-            .then(response => response.text())
-            .then(data => {
-                document.getElementById("contenidoDinamico").innerHTML = data;
-            })
-            .catch(error => console.error("Error al cargar el contenido:", error));
-    });
+    // Opcional: Reproducir automáticamente el audio (silenciado)
+    audio.play(); // Esto funciona porque el audio está configurado como "muted"
 };
